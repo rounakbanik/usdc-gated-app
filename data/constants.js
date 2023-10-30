5a4737d6fbabbd9d0588a62fa637991755c8db07
Rounak Banik <rounakbanik@gmail.com>
2023-10-30 17:04:05 +0530

    Add env

--- data/constants.js
+++ data/constants.js
@@ -1,7 +1,9 @@
+import dotenv from dotenv;
+
 // Infura API Key
-const apiKey = "<INFURA API KEY>";
+const apiKey = process.env.INFURA_API_KEY;
 
 // Circle sandbox API key
-const circleSandboxApiKey = "<CIRCLE API KEY>";
+const circleSandboxApiKey = process.env.CIRCLE_API_KEY;
 
 export { apiKey, circleSandboxApiKey }
